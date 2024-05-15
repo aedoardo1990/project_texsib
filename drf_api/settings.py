30ae1793f61a98c268f9624729c0d1f63980f42d
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-05-15 16:09:56 +0200

    save changes to settings related to cache

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -57,6 +57,13 @@ MIDDLEWARE = [
     'simple_history.middleware.HistoryRequestMiddleware',
 ]
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
+
 ROOT_URLCONF = 'drf_api.urls'
 
 TEMPLATES = [
