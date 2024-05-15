0d8ed22c109d173a50e904d13aeda878e7b57340
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-05-15 13:31:14 +0200

    change admin.py

--- goals/admin.py
+++ goals/admin.py
@@ -1,16 +1,6 @@
 from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
 from .models import Goal
 
 
-@admin.register(Goal)
-class GoalAdmin(admin.ModelAdmin):
-    list_display = [
-        'owner',
-        'department',
-        'goal_description',
-        'goal_criteria',
-        'rating',
-        'last_update',
-        'updated_at',
-        'others',
-        ]
+admin.site.register(Goal, SimpleHistoryAdmin)
