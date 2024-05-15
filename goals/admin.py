98462ce9939e94d3913e8a9f68d98faffa6e2e15
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-05-15 12:01:46 +0200

    create goals model and admn

--- goals/admin.py
+++ goals/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Goal
 
-# Register your models here.
+
+@admin.register(Goal)
+class GoalAdmin(admin.ModelAdmin):
+    list_display = [
+        'owner',
+        'department',
+        'title',
+        'goal_description',
+        'goal_criteria',
+        'rating',
+        'last_update',
+        'created_at',
+        'updated_at',
+        'others',
+        'history'
+        ]
--- goals/models.py
+++ goals/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from simple_history.models import HistoricalRecords
 
-# Create your models here.
+
+class Goal(models.Model):
+    """
+    Goal model
+    """
+    RATING = [(1), (2), (3), (4), (5), (6), (7), (8), (9), (10)]
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    department = models.CharField(max_length=255)
+    title = models.TextField(blank=True)
+    goal_description = models.TextField(blank=True)
+    goal_criteria = models.TextField(blank=True)
+    rating = models.IntegerField(choices=RATING, default=1)
+    last_update = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    others = models.TextField(blank=True)
+    history = HistoricalRecords()
+
+    class Meta:
+        ordering = ['-updated_at']
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
