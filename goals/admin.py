3840245fc99a1aab5c7cc3a7ba0a02936044367f
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-05-15 13:56:50 +0200

    save admin changes

--- goals/admin.py
+++ goals/admin.py
@@ -3,17 +3,5 @@ from simple_history.admin import SimpleHistoryAdmin
 from .models import Goal
 
 
-@admin.register(Goal)
-class GoalAdmin(admin.ModelAdmin):
-    list_display = [
-        'owner',
-        'department',
-        'goal_description',
-        'goal_criteria',
-        'rating',
-        'last_update',
-        'updated_at',
-        'others',
-        ]
-    
-admin.site.register(SimpleHistoryAdmin)
+admin.site.register(Goal, SimpleHistoryAdmin)
+
