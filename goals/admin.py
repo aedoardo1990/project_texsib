b5e5f9c0c94ded32eda5cc82dfd4d02c330a2624
Edoardo Angeli <edoardo.angeli1990@gmail.com>
2024-05-15 14:15:10 +0200

    change model goal admin

--- goals/admin.py
+++ goals/admin.py
@@ -4,8 +4,8 @@ from .models import Goal
 
 
 class GoalHistoryAdmin(SimpleHistoryAdmin):
-    list_display = ["id", "department", "title"]
-    history_list_display = ["department"]
-    search_fields = ['title', 'department']
+    list_display = ['id', 'department', 'goal_description', 'goal_criteria', 'rating', 'last_update', 'others']
+    history_list_display = ['department', 'goal_description', 'goal_criteria', 'rating', 'last_update', 'others']
+    search_fields = ['department', 'goal_description', 'goal_criteria', 'rating', 'last_update', 'others']
 
 admin.site.register(Goal, GoalHistoryAdmin)
